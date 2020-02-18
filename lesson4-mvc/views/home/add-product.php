06dbbc12560db663410e16254d656ff2309c6d50
thienth32 <thienth32@gmail.com>
2020-02-18 10:49:39 +0700

    validate form

--- lesson4-mvc/views/home/add-product.php
+++ lesson4-mvc/views/home/add-product.php
@@ -91,17 +91,57 @@
         reader.readAsDataURL(file);
     }
     $(document).ready(function(){
+
+        // tên: bắt buộc nhập, tối thiểu 2 ký tự
+        // giá: bắt buộc nhập, phải là số, không âm
+        // views: ko bắt buộc nhập, phải là số, không âm
+        // star: ko bắt buộc nhập, phải là số, không âm, nằm trong khoảng 0-5
+        // ảnh sản phẩm: bắt buộc nhập, chỉ chấp nhận định dạng ảnh
         $('#add-product-form').validate({
             rules:{
                 name: {
                     required: true,
-                    minlength: 3
+                    minlength: 2
+                },
+                price: {
+                    required: true,
+                    number: true,
+                    min: 1
+                },
+                views: {
+                    number: true,
+                    min: 0
+                },
+                star: {
+                    number: true,
+                    min: 0
+                },
+                image: {
+                    required: true,
+                    extension: "jpg|png|jpeg|gif"
                 }
             },
             messages:{
                 name: {
                     required: "Nhập tên sản phẩm",
-                    minlength: "Tên sản phẩm cần có ít nhất 3 ký tự"
+                    minlength: "Tối thiểu 2 ký tự"
+                },
+                price: {
+                    required: "Nhập giá sản phẩm",
+                    number: "Yêu cầu nhập số",
+                    min: "Giá trị nhỏ nhất là 1"
+                },
+                views: {
+                    number: "Yêu cầu nhập số",
+                    min: "Không nhập số âm"
+                },
+                star: {
+                    number: "Yêu cầu nhập số",
+                    min: "Không nhập số âm"
+                },
+                image: {
+                    required: "Hãy chọn ảnh sản phẩm",
+                    extension: "Hãy chọn file định dạng ảnh (jpg|png|jpeg|gif)"
                 }
             }
         });
