09d05a94df2068c629d621004c416b64da9a3016
thienth32 <thienth32@gmail.com>
2020-02-18 10:16:28 +0700

    jquery validate

--- lesson4-mvc/views/home/add-product.php
+++ lesson4-mvc/views/home/add-product.php
@@ -6,9 +6,8 @@
     <title>Thêm sản phẩm</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
-        .img-preview{
-            min-height: 125px;
-            border: 1px solid #ccc;
+        .form-group label.error{
+            color: indianred;
         }
     </style>
 </head>
@@ -17,7 +16,7 @@
     <br>
     <br>
     <div class="container">
-        <form action="<?= BASE_URL . 'save-add-product'?>" method="post" enctype="multipart/form-data">
+        <form id="add-product-form" action="<?= BASE_URL . 'save-add-product'?>" method="post" enctype="multipart/form-data">
             <div class="row">
                 <div class="col-6">
                     <div class="form-group">
@@ -72,11 +71,12 @@
         </form>
     </div>
 
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/additional-methods.min.js"></script>
 <script>
     function encodeImageFileAsURL(element) {
         var file = element.files[0];
@@ -91,7 +91,20 @@
         reader.readAsDataURL(file);
     }
     $(document).ready(function(){
-
+        $('#add-product-form').validate({
+            rules:{
+                name: {
+                    required: true,
+                    minlength: 3
+                }
+            },
+            messages:{
+                name: {
+                    required: "Nhập tên sản phẩm",
+                    minlength: "Tên sản phẩm cần có ít nhất 3 ký tự"
+                }
+            }
+        });
     });
 </script>
 </body>
