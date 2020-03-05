db0cddd572f772a80f570b5f17aace3a618521ee
thienth32 <thienth32@gmail.com>
2020-03-05 09:38:25 +0700

    update

--- lesson4-mvc/commons/Routing.php
+++ lesson4-mvc/commons/Routing.php
@@ -7,9 +7,25 @@
  */
 
 namespace Commons;
-
+use Phroute\Phroute\RouteCollector;
+use Phroute\Phroute\Dispatcher;
 
 class Routing
 {
+    public static function index($url){
 
+
+        $router = new RouteCollector();
+
+        $router->get('/', ["Controllers\HomeController", "index"]);
+
+        $router->get('add-product', ["Controllers\ProductController", "addForm"]);
+
+        $dispatcher = new Dispatcher($router->getData());
+
+        $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($url,
+            PHP_URL_PATH));
+
+        echo $response;
+    }
 }
--- lesson4-mvc/index.php
+++ lesson4-mvc/index.php
@@ -4,52 +4,10 @@ require_once './commons/helpers.php';
 
 require_once './vendor/autoload.php';
 
+use Commons\Routing;
+
 $url = isset($_GET['url']) == true ? $_GET['url'] : "/";
 
-use Controllers\HomeController;
-use Controllers\ProductController;
-
-switch ($url) {
-	case '/':
-		$ctr = new HomeController();
-		$ctr->index();
-		break;
-	case 'gioi-thieu':
-		$ctr = new HomeController();
-		$ctr->about();
-		break;
-	case 'chi-tiet-sp':
-		$ctr = new ProductController();
-		$ctr->detail();
-		break;
-	case 'remove-product':
-		$ctr = new ProductController();
-		$ctr->remove();
-		break;
-	case 'check-product-name':
-		$ctr = new ProductController();
-		$ctr->checkNameExisted();
-		break;
-	case 'save-add-product':
-		$ctr = new ProductController();
-		$ctr->saveAdd();
-		break;
-	case 'add-product':
-		$ctr = new ProductController();
-		$ctr->addForm();
-		break;
-	case 'edit-product':
-		$ctr = new ProductController();
-		$ctr->editForm();
-		break;
-	case 'save-edit-product':
-		$ctr = new ProductController();
-		$ctr->saveEdit();
-		break;
-	
-	default:
-		# code...
-		break;
-}
+Routing::index(trim($url));
 
  ?>
