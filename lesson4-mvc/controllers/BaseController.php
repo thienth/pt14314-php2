587590dbb6a73738750aacf03b562d4dfb0866ab
thienth32 <thienth32@gmail.com>
2020-03-06 10:26:22 +0700

    update

---
+++ lesson4-mvc/controllers/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ginv2
+ * Date: 3/6/20
+ * Time: 10:23
+ */
+
+namespace Controllers;
+
+use Jenssegers\Blade\Blade;
+class BaseController
+{
+    protected function render($viewPath, $dataArr = []){
+        $blade = new Blade('views', 'storage');
+
+        echo $blade->make('homepage', ['name' => 'John Doe'])->render();
+    }
+
+}
